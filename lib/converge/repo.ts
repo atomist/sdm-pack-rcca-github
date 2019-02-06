76d1d826f4168738f2a7b94f3afb2298b3208507
Christian Dupuis <cd@atomist.com>
2019-02-06 20:35:17 +0100

    Converge repo even without secret and existing target confi

--- lib/converge/repo.ts
+++ lib/converge/repo.ts
@@ -1,19 +1,3 @@
-/*
- * Copyright © 2019 Atomist, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import {
     GraphQL,
     Success,
@@ -41,23 +25,20 @@ export const ConvergeRepoOnRepoProvenance: EventHandlerRegistration<OnSdmRepoPro
 
         const provider = await loadProvider(ctx.graphClient, `${ctx.workspaceId}_${providerId}`);
 
-        const secret = _.get(provider, "credential.secret") as string;
-        const targetConfiguration = _.get(provider, "targetConfiguration");
+        const targetConfiguration = _.get(provider, "targetConfiguration", { orgSpecs: [], repoSpecs: [] }) as TargetConfiguration;
 
-        if (!!secret && !!targetConfiguration) {
-            const hasOrg = targetConfiguration.orgSpecs.some(o => o === owner);
-            const hasRepo = targetConfiguration.repoSpecs.some(r => r.ownerSpec === owner && r.nameSpec === repo);
-            if (!hasOrg && !hasRepo) {
-                targetConfiguration.repoSpecs.push({ ownerSpec: owner, nameSpec: repo });
-                await ctx.graphClient.mutate<ConfigureGitHubScmProvider.Mutation, ConfigureGitHubScmProvider.Variables>({
-                    name: "ConfigureGitHubScmProvider",
-                    variables: {
-                        id: providerId,
-                        orgs: targetConfiguration.orgSpecs,
-                        repos: targetConfiguration.repoSpecs.map(r => ({ owner: r.ownerSpec, repo: r.nameSpec })),
-                    },
-                });
-            }
+        const hasOrg = targetConfiguration.orgSpecs.some(o => o === owner);
+        const hasRepo = targetConfiguration.repoSpecs.some(r => r.ownerSpec === owner && r.nameSpec === repo);
+        if (!hasOrg && !hasRepo) {
+            targetConfiguration.repoSpecs.push({ ownerSpec: owner, nameSpec: repo });
+            await ctx.graphClient.mutate<ConfigureGitHubScmProvider.Mutation, ConfigureGitHubScmProvider.Variables>({
+                name: "ConfigureGitHubScmProvider",
+                variables: {
+                    id: providerId,
+                    orgs: targetConfiguration.orgSpecs,
+                    repos: targetConfiguration.repoSpecs.map(r => ({ owner: r.ownerSpec, repo: r.nameSpec })),
+                },
+            });
         }
 
         return Success;
